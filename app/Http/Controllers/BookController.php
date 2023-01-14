54291996ef85d8576dc1c03aac6e08c25539f95f
Tiago Neves Leao Costa Lino <tiagobrasil18@gmail.com>
2023-01-14 19:03:18 -0300

    Updating BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return Book::all();
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate(Book::$rules);
+        $book = Book::create($validatedData);
+        return response()->json($book, 201);
+    }
+
+    public function show($id)
+    {
+        $book = Book::findOrFail($id);
+        return $book;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate(Book::$rules);
+        $book = Book::findOrFail($id);
+        $book->update($validatedData);
+        return response()->json($book);
+    }
+
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+        return response()->json(null, 204);
+    }
 }
